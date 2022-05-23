723f26a58a4a1d00cf739f776b9aa8c7d2ae8057
Blair Bristow <blairhbristow@gmail.com>
2022-05-23 13:58:57 -0400

    First 3

--- MathApp.java
+++ MathApp.java
@@ -1,25 +1,25 @@
 public class MathApp {
   public static void main(String[] args) {
-    MathApp.maxFinder();
-    MathApp.minFinder();
-    MathApp.circleArea();
+    MathApp.maxFinder(50, 49);
+    MathApp.minFinder(49, 50);
+    MathApp.circleArea(7.25);
     MathApp.squareRoot();
     MathApp.distanceFinder();
     MathApp.absoluteValue();
     MathApp.random0To1();
-    System.out.println("Answer goes here");
   }
 
-  private static void maxFinder() {
-
+  private static void maxFinder(double job1Salary, double job2Salary) {
+    System.out.println(Math.max(job1Salary, job2Salary));
   }
 
-  private static void minFinder() {
-
+  private static void minFinder(double carPrice, double truckPrice) {
+    System.out.println(Math.min(carPrice, truckPrice));
   }
   
-  private static void circleArea() {
-
+  private static void circleArea(double radius) {
+    double area = radius * radius * Math.PI;
+    System.out.println(area);
   }
 
   private static void squareRoot() {
