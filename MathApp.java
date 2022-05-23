7435dcb6c945ae5edcf0a84ef78aeff84294a7f6
Blair Bristow <blairhbristow@gmail.com>
2022-05-23 13:38:46 -0400

    Declare formulas

--- MathApp.java
+++ MathApp.java
@@ -1,5 +1,42 @@
 public class MathApp {
   public static void main(String[] args) {
+    MathApp.maxFinder();
+    MathApp.minFinder();
+    MathApp.circleArea();
+    MathApp.squareRoot();
+    MathApp.distanceFinder();
+    MathApp.absoluteValue();
+    MathApp.random0To1();
     System.out.println("Answer goes here");
   }
+
+  private static void maxFinder() {
+
+  }
+
+  private static void minFinder() {
+
+  }
+  
+  private static void circleArea() {
+
+  }
+
+  private static void squareRoot() {
+
+  }
+  
+  private static void distanceFinder() {
+
+  }
+  
+  private static void absoluteValue() {
+
+  }
+  
+  private static void random0To1() {
+
+  }
+  
+
 }
